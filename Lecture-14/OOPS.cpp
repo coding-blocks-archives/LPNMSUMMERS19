3f0f40e3eaa8111cfbd4e35c3441c4780abb2084
kartik <kartikmathur@MacBook-Pro.local>
2019-06-14 11:29:00 +0530

    Lecture-14

--- Lecture-14/OOPS.cpp
+++ Lecture-14/OOPS.cpp
@@ -6,40 +6,43 @@ class Car{
 private:
 	int price;
 public:
+	static int count;
 	char* name;
 	int model;
 	int seats;
+	const int tyres;
 
 	// Default Constructor
-	Car(){
+	Car():tyres(4){
 		name = NULL;
 		cout<<"In Default Constructor"<<endl;
+		count++;
 	}
 
 	// Parameterzied constructor
-	Car(char* n,int p,int s,int m){
+	Car(char* n,int p,int s,int m):tyres(4),price(p),seats(s),model(m){
 		cout<<"In Parameterzied Constructor"<<endl;
 		name = new char[strlen(n)+1];
 		strcpy(name,n);
-		price = p;
-		seats = s;
-		model = m;
+		count++;
 	}
 
 	// Copy Constructor
-	Car(Car &X){
+	Car(Car &X):tyres(4){
 		cout<<"In Copy Constructor"<<endl;
 		price = X.price;
 		seats = X.seats;
 		model = X.model;
 		name = new char[strlen(X.name)+1];
 		strcpy(name,X.name);
+		count++;
 	}
 
-	void print(){
+	void print()const{
 		cout<<"Name  : "<<name<<endl;
 		cout<<"Price : "<<price<<endl;
 		cout<<"Model : "<<model<<endl;
+		cout<<"Tyres : "<<tyres<<endl;
 		cout<<"Seats : "<<seats<<endl<<endl;
 	}
 
@@ -52,7 +55,7 @@ public:
 		}
 	}
 
-	int GetPrice(){
+	int GetPrice()const{
 		return price;
 	}
 
@@ -78,10 +81,19 @@ public:
 		seats = X.seats;
 		model = X.model;
 	}
+
+	// Destructor
+	~Car(){
+		cout<<"In Destructor"<<name<<endl;
+		count--;
+	}
 };
+
+int Car::count;
 // ---------------------------------------------
 
 int main(){
+
 	Car a;
 
 	// strcpy(a.name,"Maruti");
@@ -107,11 +119,23 @@ int main(){
 	Car e; 
 	e = d; // --> this calls copy assignment
 	e.name[0]='T';
+	int * m = new int;
+	float * f = new float;
+	Car* g = new Car;
+	(*g).name = new char [10];
+	strcpy((*g).name,"F1");
+	cout<<(*g).name<<endl;
 	a.print();
 	b.print();
 	c.print();
 	d.print();
 	e.print();
+	(*g).print();
+	cout<<"No of cars "<<a.count<<endl;
+	delete g;
+	g= NULL;
+	cout<<"No of cars "<<Car::count<<endl;
+
 
 
 
